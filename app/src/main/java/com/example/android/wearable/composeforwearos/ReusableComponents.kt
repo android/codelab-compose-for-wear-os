350adf671de18f5d7caa14362ad5c60b844f4ba7
Jeremy Walker <jewalker@google.com>
2021-10-12 18:14:18 -0700

    Initial draft of complete module for Compose for Wear OS code lab.
    Change-Id: I8a35271b99fbf1f1c538f47cea8f472cf5b6a95b

---
+++ app/src/main/java/com/example/android/wearable/composeforwearos/ReusableComponents.kt
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.wearable.composeforwearos
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Message
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.SelfImprovement
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.AppCard
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.ToggleChip
+import androidx.wear.compose.material.ToggleChipDefaults
+
+/* Contains individual Wear OS demo composables for the code lab. */
+
+@Composable
+fun ButtonExample(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    // TODO: Create a Button Composable (with a Row to center)
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        // Button
+        Button(
+            modifier = Modifier.size(ButtonDefaults.LargeButtonSize),
+            onClick = { /* ... */ },
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Phone,
+                contentDescription = "Button triggers phone action",
+                modifier = iconModifier
+            )
+        }
+    }
+}
+
+@Composable
+fun TextExample(modifier: Modifier = Modifier) {
+    // TODO: Create a Text Composable
+    Text(
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.device_shape)
+    )
+}
+
+@Composable
+fun CardExample(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    // TODO: Create a Card (specifically, an AppCard) Composable
+    AppCard(
+        modifier = modifier,
+        appImage = {
+            Icon(
+                imageVector = Icons.Rounded.Message,
+                contentDescription = "Card triggers open message action",
+                modifier = iconModifier
+            )
+        },
+        appName = { Text("Messages") },
+        time = { Text("12m") },
+        title = { Text("Kim Green") },
+        onClick = { /* ... */ }
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text("On my way!")
+        }
+    }
+}
+
+@Composable
+fun ChipExample(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    // TODO: Create a Chip Composable
+    Chip(
+        modifier = modifier,
+        onClick = { /* ... */ },
+        label = {
+            Text(
+                text = "5 minute Meditation",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        icon = {
+            Icon(
+                imageVector = Icons.Rounded.SelfImprovement,
+                contentDescription = "Chip triggers meditation action",
+                modifier = iconModifier
+            )
+        },
+    )
+}
+
+@Composable
+fun ToggleChipExample(modifier: Modifier = Modifier) {
+    // TODO: Create a ToggleChip Composable
+    var checked by remember { mutableStateOf(true) }
+    ToggleChip(
+        modifier = modifier,
+        checked = checked,
+        toggleIcon = {
+            ToggleChipDefaults.SwitchIcon(checked = checked)
+        },
+        onCheckedChange = {
+            checked = it
+        },
+        label = {
+            Text(
+                text = "Sound",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    )
+}
+
+// Function only used as a demo for when you start the code lab (removed as step 1).
+@Composable
+fun StartOnlyTextComposables() {
+    Text(
+        modifier = Modifier.fillMaxSize(),
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.hello_world_starter)
+    )
+}
